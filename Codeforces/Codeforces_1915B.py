e79ed632f284ab3812919bf2628b31a9cd3b26fd
SRR23 <srrimon267@gmail.com>
2023-12-29 23:36:52 +0600

    added

--- Codeforces/Codeforces_1915B.py
+++ Codeforces/Codeforces_1915B.py
@@ -1,4 +1,17 @@
+# Follow Codeforce_id xyyxyxxy 
 
 t=int(input())
 while t>0:
     t-=1
+    a=input()
+    b=input()
+    c=input()
+    
+    x=ord(a[0])+ord(a[1])+ord(a[2])
+    y=ord(b[0])+ord(b[1])+ord(b[2])
+    z=ord(c[0])+ord(c[1])+ord(c[2])
+    
+    ans=(x+y+z)-ord('?')-198*3
+    
+    print(chr(-ans))
+    
